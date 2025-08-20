3ad4368c836dbadd4034bb86594a1bc679f4d681
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2025-08-20 19:42:02 +0200

    Slight modified a ver of gnl

--- originals/get_next_line/bs-ver./t2/get_next_line.c
+++ originals/get_next_line/bs-ver./t2/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 16:34:19 by jegerman          #+#    #+#             */
-/*   Updated: 2025/06/07 16:47:33 by jegerman         ###   ########.fr       */
+/*   Updated: 2025/08/20 19:19:17 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 char	*get_next_line(int fd)
 {
 	char	*line, *buffer;
-	
-	if (fd < 0
-		|| BUFFER_SIZE < 1
+
+	if (BUFFER_SIZE < 1 || fd < 0
 		|| !(line = malloc(20 * 1000))
 		|| !(buffer = line))
 		return (NULL);
-	while ((read(fd, buffer, 1) > 0) && (*buffer++ != '\n'))
+	while (read(fd, buffer, 1) > 0
+		&& *buffer++ != '\n')
 		;
-	return ((buffer > line) ? (*buffer = 0, line): (free(line), NULL));
+	return (buffer > line ? (*buffer = 0, line) : (free(line), NULL));
 }

3a1701ef0f3657d3d2311f764dc89bebe914ece9
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2025-08-26 17:48:52 +0200

    Snapshot of the least fonctional state of gpt's sorted-permutations

--- backtracking/sorted-permutations/t1/permutations.c
+++ backtracking/sorted-permutations/t1/permutations.c
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 17:55:25 by jegerman          #+#    #+#             */
-/*   Updated: 2025/08/26 17:26:26 by jegerman         ###   ########.fr       */
+/*   Updated: 2025/08/26 17:47:30 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,22 @@
 #include <string.h>
 #include <stdlib.h>
 
-static void dfs(int level, int n, int cnt[256], char *buf) {
-    if (level == n)
+static void dfs(int level, int n, int cnt[256], char *buf)
+{
+	if (level == n)
 	{
-        puts(buf);
-        return;
-    }
-    for (int c = 0; c < 256; ++c){          // ASCII order
-        if (cnt[c] > 0) {
-            cnt[c]--;
-            buf[level] = (char)c;
-            dfs(level + 1, n, cnt, buf);
-            cnt[c]++;
-        }
+		puts(buf);
+		return;
+	}
+	for (int c = 0; c < 256; ++c)
+	{
+		if (cnt[c] > 0)
+		{
+			cnt[c]--;
+			buf[level] = (char)c;
+			dfs(level + 1, n, cnt, buf);
+			cnt[c]++;
+		}
     }
 }
 
@@ -34,20 +37,20 @@ void	permute_ascii_order(char *comb)
 {
     int	comb_len;
     int	cnt[256];
-	// char *buffer;
-
+	
 	comb_len = strlen(comb);
-	cnt[0] = 0;
 
-	for (unsigned char *p = comb; *p; ++p)
+
+	for (int i = 0 ; i < 256; i++)
+		cnt[i] = 0;
+
+	for (const unsigned char *p = (const unsigned char*)comb; *p; ++p)
 		cnt[*p]++;
 
-
-	
-	// buffer = malloc(comb_len + 1);
-    // buffer[comb_len] = '\0';
-    dfs(0, comb_len, cnt, comb);
-    // free(buffer);
+	char *buf = (char*)malloc(comb_len + 1);
+	buf[comb_len] = '\0';
+    dfs(0, comb_len, cnt, buf);
+	free(buf);
 }
 
 int main(int argc, char **argv)

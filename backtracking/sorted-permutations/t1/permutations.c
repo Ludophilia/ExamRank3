b584bfac3bf96547cc9024876b5a0dc6a9b6c071
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2025-08-26 19:20:59 +0200

    Improved and simplified gpt sorted-permutation solution

--- backtracking/sorted-permutations/t1/permutations.c
+++ backtracking/sorted-permutations/t1/permutations.c
@@ -6,57 +6,46 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 17:55:25 by jegerman          #+#    #+#             */
-/*   Updated: 2025/08/26 17:47:30 by jegerman         ###   ########.fr       */
+/*   Updated: 2025/08/26 19:12:23 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-static void dfs(int level, int n, int cnt[256], char *buf)
+static int	ft_strlen(char *str)
 {
-	if (level == n)
-	{
-		puts(buf);
-		return;
-	}
-	for (int c = 0; c < 256; ++c)
-	{
-		if (cnt[c] > 0)
-		{
-			cnt[c]--;
-			buf[level] = (char)c;
-			dfs(level + 1, n, cnt, buf);
-			cnt[c]++;
-		}
-    }
+	int	len;
+
+	len = 0;
+	while (str[len])
+		++len;
+	return (len);
 }
 
-void	permute_ascii_order(char *comb)
+static void permutations(int at, char *comb, int len, int cfreq[256])
 {
-    int	comb_len;
-    int	cnt[256];
-	
-	comb_len = strlen(comb);
-
-
-	for (int i = 0 ; i < 256; i++)
-		cnt[i] = 0;
-
-	for (const unsigned char *p = (const unsigned char*)comb; *p; ++p)
-		cnt[*p]++;
-
-	char *buf = (char*)malloc(comb_len + 1);
-	buf[comb_len] = '\0';
-    dfs(0, comb_len, cnt, buf);
-	free(buf);
+	if (at == len && puts(comb))
+		return;
+	for (int c = 0; c < 256; c++)
+	{
+		if (cfreq[c] > 0)
+		{
+			cfreq[c]--;
+			comb[at] = c;
+			permutations(at + 1, comb, len, cfreq);
+			cfreq[c]++;
+		}
+	}
 }
 
 int main(int argc, char **argv)
 {
+	int	cfrq[256] = {0}; // The histogram.
+
 	if (++argv, --argc != 1)
 		return (1);
-    permute_ascii_order(*argv);
-    return 0;
+	for (unsigned char *p = (unsigned char*)*argv; *p; p++)
+		cfrq[*p]++;
+	permutations(0, *argv, ft_strlen(*argv), cfrq);
+	return (0);
 }

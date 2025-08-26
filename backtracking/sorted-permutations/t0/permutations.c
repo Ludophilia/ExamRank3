0d96ccf019170a9d718a51a8a6224baba1c4105f
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2025-08-26 16:24:54 +0200

    Done sorted-permutations

--- backtracking/sorted-permutations/t0/permutations.c
+++ backtracking/sorted-permutations/t0/permutations.c
@@ -6,13 +6,14 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 17:55:25 by jegerman          #+#    #+#             */
-/*   Updated: 2025/08/25 22:15:00 by jegerman         ###   ########.fr       */
+/*   Updated: 2025/08/26 16:12:44 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 # define SWAP(a, b, T) do { T _t = (a); (a) = (b); (b) = _t; } while (0)
 
 int		ft_strlen(char *str)
@@ -44,6 +45,7 @@ int	permutations(int at, char *str, char **sols, int *spos)
 	if (str[at + 1] == 0)
 	{
 		sols[*spos] = ft_strdup(str);
+		sols[*spos + 1] = 0;
 		*spos += 1;
 		return (0);
 	}
@@ -52,6 +54,29 @@ int	permutations(int at, char *str, char **sols, int *spos)
 		SWAP(str[at], str[j], char);
 		permutations(at + 1, str, sols, spos);
 		SWAP(str[at], str[j], char);
+	}
+	return (0);
+}
+
+int	ft_strcmp(char *str1, char *str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] && str1[i] == str2[i])
+		i++;
+	return (str1[i] - str2[i]);
+}
+
+int	sort(char **sols)
+{
+	for (int i = 0; sols[i + 1]; i++)
+	{
+		for (int j = i; sols[j]; j++)
+		{
+			if (ft_strcmp(sols[i], sols[j]) > 0)
+		 		SWAP(sols[i], sols[j], char *);
+		}
 	}
 	return (0);
 }
@@ -65,7 +90,7 @@ int	main(int argc, char **argv)
 		return (1);
 	spos = 0;
 	permutations(0, *argv, sols, &spos);
-	// sort
+	sort(sols);
 	for (int i = 0; sols[i]; i++)
 	{
 		puts(sols[i]);
